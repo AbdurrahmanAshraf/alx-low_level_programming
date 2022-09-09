2fab9e7b71ef5d4aa8f77532c33e536c01d9e1c5
Abdurrahman Ashraf <abdoshuruf1@gmail.com>
2022-09-09 11:25:35 -0700

    Added script for task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /*
  * This program prints "Programming is like building a multilingual puzzle
  */
-int main(void)
+int main(void) /** for puts */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
