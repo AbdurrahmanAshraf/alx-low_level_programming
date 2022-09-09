d163b84f931fafc19351f7c8361da506c8129a9e
Abdurrahman Ashraf <abdoshuruf1@gmail.com>
2022-09-09 11:11:06 -0700

    Edited the script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
-int main() {
-  puts ("\"Programming is like building a multilingual puzzle");
-  return 0;
-   }
+/*
+ * This program prints "Programming is like building a multilingual puzzle
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
