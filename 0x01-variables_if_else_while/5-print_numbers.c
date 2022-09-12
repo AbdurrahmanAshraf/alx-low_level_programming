10f52537e089e00e18c719f3d069ea0c2980b4b0
Abdurrahman Ashraf <abdoshuruf1@gmail.com>
2022-09-12 02:46:41 -0700

    Added code of task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print all single digit numbers of base 10
+ *
+ * Return: numbers from 0 to 9
+ */
+int main(void)
+{
+  /**
+   *@x: integer type
+   */
+
+int x = 0;
+while (x <= 10)
+{
+printf("%d", x);
+x++;
+}
+return (0);
+}
