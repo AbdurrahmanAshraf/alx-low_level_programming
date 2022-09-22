477f605ebdcabc25bbba915b0ac5e8d5905cf8e6
Abdurrahman Ashraf <abdoshuruf1@gmail.com>
2022-09-21 22:35:18 -0700

    Added task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - fuction returns the length of a string
+ * @s: input string
+ * Reutrn: length of a string
+ */
+
+int _strlen(char *s)
+{
+int c = 0;
+while (*(s + count) != '\0')
+c++;
+return (c);
+}
