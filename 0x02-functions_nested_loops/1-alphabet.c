4ffabacc7a4aac1e2c52c5c20d018aabfc26a5b7
Abdurrahman Ashraf <abdoshuruf1@gmail.com>
2022-09-22 00:11:32 -0700

    Added task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
