399732d1068ca65abeb7140934fdc2b516bc9188
Abdurrahman Ashraf <65561308+AbdurrahmanAshraf@users.noreply.github.com>
2022-09-23 04:13:17 +0200

    Update task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ *@i: first integer
+ *@k: second integer
+ * Return: result of add
+ */
+
+int add(int i, int k)
+{
+return (i + k);
+}
